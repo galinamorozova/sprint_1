651aa761be4360646ee5306cfcc03b5dd432bf4a
galinamorozova <sidh_g@mail.ru>
2022-10-18 20:40:44 +0300

    del

--- precompile/src/pages/page2/index.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from 'handlebars';
-import tpl from './tpl.hbs';
-import './style.scss';
-
-Handlebars.registerPartial('page2', tpl);
-
-export default (props = {}) => {
-	return tpl(props);
-}
